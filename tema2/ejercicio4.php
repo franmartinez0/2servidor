26cf7ce96b450c51d42d668c58db88471a8ff2af
franmartinez0 <fran.martinezlopez29398@gmail.com>
2022-10-22 20:37:33 +0200

    tema3

--- tema2/ejercicio4.php
+++ tema2/ejercicio4.php
@@ -10,37 +10,19 @@
 
 <body>
     <?php
-    /*
-    Vamos a construir un encriptador y desencriptador de mensajes. Crearemos dos
-    funciones:
-    - encriptar($mensaje,$clave)
-    o donde el primer argumento sea el mensaje a encriptar
-    o el segundo argumento sea el número de letras a desplazar a la
-    derecha por cada letra, por ejemplo, la b con $clave=3 se
-    transformará en en la f.
-    o La función devolverá el mensaje cifrado sustituyendo los espacios
-    en blanco del final y cada letra del mensaje por la correspondiente
-    según la clave.
-    - desencriptar($mensaje,$clave)
-    o donde el primer argumento sea el mensaje a encriptar
-    o el segundo argumento sea el número de letras a desplazar a la
-    izquierda por cada letra, por ejemplo, la f con $clave=3 se
-    transformará en en la b.
-    o La función devolverá el mensaje cifrado sustituyendo cada letra del
-    mensaje por la correspondiente según la clave.
-    Para mostrar que lo has hecho bien encripta un mensaje y muéstralo, desencríptalo y
-    muestra el mensaje que coincide con el original. Pista: utilizar las funciones PHP para
-    pasar un carácter a su correspondiente dígito ASCII y al revés
-*/
+    
 
     function encriptar($mensaje, $clave)
     {
+
+        //divide el string en partes de 1 en 1
         $letra = str_split($mensaje, 1);
 
+        //cada valor es variado tras pasarlo al codigo ascii
         foreach ($letra as $valor) {
             $num = ord($valor);
             $nuevo = chr($num + $clave);
-
+            //mostramos el texto encriptado
             echo $nuevo;
         }
         echo "<br>";
@@ -49,12 +31,13 @@
 
     function desencriptar($mensaje, $clave)
     {
+        //volvemos a separar el codigo de 1 en 1
         $letra = str_split($mensaje, 1);
-
+        //variamos el codigo ascii de cada uno para poder descifrar el codigo cifrado anteriormente
         foreach ($letra as $valor) {
             $num = ord($valor);
             $des = chr($num - $clave);
-
+            //mostramos codigo desencriptado
             echo $des;
         }
         echo "<br>";
--- tema2/ejercicio5.php
+++ tema2/ejercicio5.php
@@ -1,48 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ejercicio5</title>
 </head>
+
 <body>
     <?php
- function encriptar($mensaje, $clave)
+    function encriptar($mensaje, $clave)
     {
+        //muestra mensaje en orden normal
         echo $mensaje;
         echo "<br>";
+        //muestra mensaje al contrario
+
         $contrario = strrev($mensaje);
         echo $contrario;
+
         echo "<br>";
-        $letras = str_split($contrario, 1);
 
 
 
-        
-            $letra = str_split($mensaje, 1);
-    
-            foreach ($letra as $valor) {
-                $num = ord($valor);
-                $nuevo = chr($num + $clave);
-    
-                echo $nuevo;
-            }
-            echo "<br>";
-        
-        
+
+
+        //dividimos el string de 1 en 1
+
+
+        $letra = str_split($mensaje, 1);
+        //encriptamos
+        foreach ($letra as $valor) {
+            $num = ord($valor);
+            $nuevo = chr($num + $clave);
+
+            echo $nuevo;
+        }
+        echo "<br>";
     }
 
     function desencriptar($mensaje, $clave)
     {
+        //mostarmos texto encriptado
         echo $mensaje;
         echo "<br>";
+
+
+        //hacemos que el string esté al contrario y mostramos texto encriptado al contrario
         $contrario = strrev($mensaje);
         echo $contrario;
         echo "<br>";
-        $letras = str_split($contrario, 1);
+
+
+        //dividimos el string en partes de 1 en 1
 
         $letra = str_split($mensaje, 1);
+        //utilizamos la funcion de desencriptar
 
         foreach ($letra as $valor) {
             $num = ord($valor);
@@ -52,11 +66,19 @@
         }
         echo "<br>";
     }
+
+
+
+
+
+
+
     echo encriptar("esto esta encriptado despues de escribirlo del reves", 1);
     echo "<br>";
     echo desencriptar("ftup!ftub!fodsjqubep!eftqvft!ef!ftdsjcjsmp!efm!sfwft", 1);
-        
+
     ?>
-    
+
 </body>
+
 </html>
