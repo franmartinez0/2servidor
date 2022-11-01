e906d5362fd9d05d2c3862c405cbda36d628920a
franmartinez0 <fran.martinezlopez29398@gmail.com>
2022-11-01 18:09:32 +0100

    retoque

--- tema3/practica1/plantilla/proyectos.php
+++ tema3/practica1/plantilla/proyectos.php
@@ -12,38 +12,31 @@ include("cabecera.php");
 
 $proyectos = array(
     [
-        "id" => 0, "nombre" => "Robot", "fechaini" => "2015", "fechafin" => "2024",
-        "dias" => "20", "porcentaje" => 40,
+        "id" => 0, "nombre" => "Robot", "fechaini" => "2015", "fechafin" => "2024", "porcentaje" => 40,
         "importancia" => 5
     ],
     [
-        "id" => 1, "nombre" => "Dron acuatico", "fechaini" => "2018", "fechafin" => "2023",
-        "dias" => "85", "porcentaje" => 90,
+        "id" => 1, "nombre" => "Dron acuatico", "fechaini" => "2018", "fechafin" => "2023","porcentaje" => 90,
         "importancia" => 2
     ],
     [
-        "id" => 2, "nombre" => "Maquina corte lechuga", "fechaini" => "2009", "fechafin" => "2026",
-        "dias" => "78", "porcentaje" => 98,
+        "id" => 2, "nombre" => "Maquina corte lechuga", "fechaini" => "2009", "fechafin" => "2026","porcentaje" => 98,
         "importancia" => 3
     ],
     [
-        "id" => 3, "nombre" => "arduino clima", "fechaini" => "2020", "fechafin" => "2022",
-        "dias" => "45", "porcentaje" => 90,
+        "id" => 3, "nombre" => "arduino clima", "fechaini" => "2020", "fechafin" => "2022","porcentaje" => 90,
         "importancia" => 2
     ],
     [
-        "id" => 4, "nombre" => "intranet empresa", "fechaini" => "2022", "fechafin" => "2022",
-        "dias" => "10", "porcentaje" => 90,
+        "id" => 4, "nombre" => "intranet empresa", "fechaini" => "2022", "fechafin" => "2022", "porcentaje" => 90,
         "importancia" => 3
     ],
     [
-        "id" => 5, "nombre" => "ordenador carga solar", "fechaini" => "2022", "fechafin" => "2026",
-        "dias" => "3", "porcentaje" => 20,
+        "id" => 5, "nombre" => "ordenador carga solar", "fechaini" => "2022", "fechafin" => "2026", "porcentaje" => 20,
         "importancia" => 3
     ],
     [
-        "id" => 6, "nombre" => "lector de huellas laboral", "fechaini" => "2022", "fechafin" => "2022",
-        "dias" => "10", "porcentaje" => 85,
+        "id" => 6, "nombre" => "lector de huellas laboral", "fechaini" => "2022", "fechafin" => "2022", "porcentaje" => 85,
         "importancia" => 4
     ],
 );
@@ -63,7 +56,6 @@ echo "<tr>";
 echo "<td>" . "NOMBRE" . "</td>";
 echo "<td>" . "FECHA INICIO" . "</td>";
 echo "<td>" . "FECHA FINAL" . "</td>";
-echo "<td>" . "DIAS TRANSCURRIDOS" . "</td>";
 echo "<td>" . "PORCENTAJE COMPLETADO" . "</td>";
 echo "<td>" . "IMPORTANCIA" . "</td>";
 echo "</thead>";
@@ -77,17 +69,16 @@ foreach ($_SESSION['proyectos'] as $valor) {
     echo "<td>" . $valor["nombre"] . "</td>";
     echo "<td>" . $valor["fechaini"] . "</td>";
     echo "<td>" . $valor["fechafin"] . "</td>";
-    echo "<td>" . $valor["dias"] . "</td>";
     echo "<td>" . $valor["porcentaje"] . "</td>";
     echo "<td>" . $valor["importancia"] . "</td>";
 
 
     //añadimos el boton de borrar para poder eliminar por id
     echo "<td><a href='controlador.php?accion=borrarid&id=" . $valor['id'] . "'
-    type='botton' name='borrarporid' class='btn btn-danger'>Borrar
+    type='botton' name='borrarporid' class='btn btn-default btn-lg active'>Borrar
     </a></td>";
     echo "<td><a href='controlador.php?accion=informacion&id=" . $valor['id'] . "'
-    type='botton' name='informacion' class='btn btn-danger'>Info
+    type='botton' name='informacion' class='btn btn-primary btn-lg active'>Info
     </button></td>";
     echo "</tr>";
 }
