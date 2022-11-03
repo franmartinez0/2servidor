e9d23373a634ef7fe8376d74b306bcbc7021a5ee
franmartinez0 <fran.martinezlopez29398@gmail.com>
2022-11-03 10:23:09 +0100

    ...

--- tema3/practica1/plantilla/verProyecto.php
+++ tema3/practica1/plantilla/verProyecto.php
@@ -26,6 +26,7 @@ echo '
         </div>
     </div>
 </div>
+<br>
 <!-- Earnings (Monthly) Card Example -->
 <div class="col-xl-3 col-md-6 mb-4">
     <div class="card border-left-primary shadow h-100 py-2">
@@ -43,6 +44,7 @@ echo '
         </div>
     </div>
 </div>
+<br>
 
 </div>
 <div class="col-xl-3 col-md-6 mb-4">
@@ -72,6 +74,7 @@ echo '
         </div>
     </div>
 </div>
+<br>
 <!-- Earnings (Monthly) Card Example -->
 <!-- Pending Requests Card Example -->
 <div class="col-xl-3 col-md-6 mb-4">
@@ -91,30 +94,11 @@ echo '
     </div>
 </div>
 </div>
-<div
-    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-    <h6 class="m-0 font-weight-bold text-primary">ESTADÍSTICAS</h6>
-</div>
-<br>
-<!-- Donut Chart -->
-<div class="col-xl-4 col-lg-5">
-    <div class="card shadow mb-4">
-        <!-- Card Header - Dropdown -->
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">AVANCE DEL PROYECTO</h6>
-        </div>
-        <!-- Card Body -->
-        <div class="card-body">
-            <div class="chart-pie pt-4">
-                <canvas id="donut"></canvas>
-            </div>
-            <hr> <center>' . strtoupper($_SESSION['ids']['nombre']) . '</center></code>
-        </div>
-    </div>
+
 </div>
 </div>
 </div>
-';
+'
 
 
 
