b126ef878964ff752bed319d933a67be8706d206
franmartinez0 <fran.martinezlopez29398@gmail.com>
2022-11-28 21:54:02 +0100

    modelo

---
+++ tema6/biblioteca/modelos/libro.php
@@ -0,0 +1,257 @@
+<?php
+ class Pelicula {
+
+    private $idLibro;
+    private $isbn;
+    private $titulo;
+    private $subtitulo;
+    private $descripcion;
+    private $autor;
+    private $editorial;
+    private $categoria;
+    private $imagen;
+    private $stock;
+    private $disponible;
+   
+
+
+    public function __construct($isbn="",$titulo="",$subtitulo="",$descripcion="",$autor="",$editorial="",$categoria="",$imagen="",$stock="",$disponible=""){
+        $this->isbn=$isbn;
+        $this->titulo=$titulo;
+        $this->subtitulo=$subtitulo;
+        $this->descripcion=$descripcion;
+        $this->autor=$autor;
+        $this->editorial=$editorial;
+        $this->categoria=$categoria;
+        $this->imagen=$imagen;
+        $this->stock=$stock;
+        $this->disponible=$disponible;
+
+    }
+
+
+    
+
+    
+
+
+    /**
+     * Get the value of idLibro
+     */ 
+    public function getIdLibro()
+    {
+        return $this->idLibro;
+    }
+
+    /**
+     * Set the value of idLibro
+     *
+     * @return  self
+     */ 
+    public function setIdLibro($idLibro)
+    {
+        $this->idLibro = $idLibro;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of isbn
+     */ 
+    public function getIsbn()
+    {
+        return $this->isbn;
+    }
+
+    /**
+     * Set the value of isbn
+     *
+     * @return  self
+     */ 
+    public function setIsbn($isbn)
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of titulo
+     */ 
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * Set the value of titulo
+     *
+     * @return  self
+     */ 
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of subtitulo
+     */ 
+    public function getSubtitulo()
+    {
+        return $this->subtitulo;
+    }
+
+    /**
+     * Set the value of subtitulo
+     *
+     * @return  self
+     */ 
+    public function setSubtitulo($subtitulo)
+    {
+        $this->subtitulo = $subtitulo;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of descripcion
+     */ 
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    /**
+     * Set the value of descripcion
+     *
+     * @return  self
+     */ 
+    public function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of autor
+     */ 
+    public function getAutor()
+    {
+        return $this->autor;
+    }
+
+    /**
+     * Set the value of autor
+     *
+     * @return  self
+     */ 
+    public function setAutor($autor)
+    {
+        $this->autor = $autor;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of editorial
+     */ 
+    public function getEditorial()
+    {
+        return $this->editorial;
+    }
+
+    /**
+     * Set the value of editorial
+     *
+     * @return  self
+     */ 
+    public function setEditorial($editorial)
+    {
+        $this->editorial = $editorial;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of categoria
+     */ 
+    public function getCategoria()
+    {
+        return $this->categoria;
+    }
+
+    /**
+     * Set the value of categoria
+     *
+     * @return  self
+     */ 
+    public function setCategoria($categoria)
+    {
+        $this->categoria = $categoria;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of imagen
+     */ 
+    public function getImagen()
+    {
+        return $this->imagen;
+    }
+
+    /**
+     * Set the value of imagen
+     *
+     * @return  self
+     */ 
+    public function setImagen($imagen)
+    {
+        $this->imagen = $imagen;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of stock
+     */ 
+    public function getStock()
+    {
+        return $this->stock;
+    }
+
+    /**
+     * Set the value of stock
+     *
+     * @return  self
+     */ 
+    public function setStock($stock)
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of disponible
+     */ 
+    public function getDisponible()
+    {
+        return $this->disponible;
+    }
+
+    /**
+     * Set the value of disponible
+     *
+     * @return  self
+     */ 
+    public function setDisponible($disponible)
+    {
+        $this->disponible = $disponible;
+
+        return $this;
+    }
+ }
